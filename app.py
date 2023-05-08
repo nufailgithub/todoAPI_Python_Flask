a9cdc5522529f9614c39c9b37e410398bfa998e0
Mohamed Nufail <nufailmohamed07@gmail.com>
2023-05-08 23:00:58 +0530

    make some changes in config, change the login funtion POST endpoint, add the token creation inside the POST method

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask,jsonify, request, make_response
-from flask_sqlalchemy import SQLAlchemy
-import os
-import jwt
+from flask_sqlalchemy import SQLAlchemy    #To SQLlite DB
+import os       #to set tha path for DB file
+import jwt      #to create json web token
+import datetime  #to set exp in generate web token
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "thisIsSecret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(app.root_path,'todos.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
 db = SQLAlchemy(app)
 
 
@@ -15,26 +19,24 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean, default=False)
 
-@app.route('/')
+@app.route('/todos', methods=['POST'])
 def login():
     auth = request.authorization
 
     if auth.password == 'password':
-        return "You are logged in"
-    return make_response('Could not verify!', 401, {"WWW-Authenticate": 'Basic realm="Login Required'})
+        token = jwt.encode({'id':user.id, 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+        data = request.get_json()
 
+        new_task = Task(id=data['id'], title=data['title'], complete=False)
 
-@app.route('/task', methods=['POST'])
-def create_task():
-    data = request.get_json()
+        db.session.add(new_task)
+        db.session.commit()
 
-    new_task = Task(id=data['id'], title=data['title'], complete=False)
+        return make_response(jsonify("New task is created!"), 200)
 
-    db.session.add(new_task)
-    db.session.commit()
-
-    return make_response(jsonify("New task is created!"), 200)
-
+    else:
+        return make_response('Could not verify!', 401, {"WWW-Authenticate": 'Basic realm="Login Required'})
 
 @app.route('/todos', methods=['GET'])
 def get_todos():
@@ -47,7 +49,6 @@ def get_todos():
             'complete': todo.complete,
         })
     return jsonify(result)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
